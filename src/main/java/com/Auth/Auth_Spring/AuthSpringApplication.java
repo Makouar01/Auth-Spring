c37cb30fbdabb68b104aea0d3cf43c82d54b7a46
“Makouar“ <abderahimmakouar@gmail.com>
2024-09-30 22:25:33 +0100

    Premier commit

---
+++ src/main/java/com/Auth/Auth_Spring/AuthSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Auth.Auth_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Auth/Auth_Spring/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auth.Auth_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
